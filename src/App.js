a326eacb7c548a83fc80b6da311985a2e1a3d5b3
oliverclaxton <oliver.claxton66@gmail.com>
2020-11-23 17:32:25 +0100

    initial set up of react and redux store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+
+import { Switch, Route } from "react-router-dom";
+// import Navigation from "./components/Navigation";
+// import Loading from "./components/Loading";
+// import MessageBox from "./components/MessageBox";
+// import SignUp from "./pages/SignUp";
+// import Login from "./pages/Login";
+// import MySpace from "./pages/MySpace";
+// import Spaces from "./pages/Spaces";
+// import SpaceDetails from "./pages/SpaceDetails";
+
+// import { useDispatch, useSelector } from "react-redux";
+// import { selectAppLoading } from "./store/appState/selectors";
+// import { getUserWithStoredToken } from "./store/user/actions";
 
 function App() {
+  // const dispatch = useDispatch();
+  // const isLoading = useSelector(selectAppLoading);
+
+  // useEffect(() => {
+  //   dispatch(getUserWithStoredToken());
+  // }, [dispatch]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Navigation />
+      <MessageBox /> */}
+      <h1>HELLO TALENT HUB</h1>
+      <Switch>
+        {/* {isLoading ? <Loading /> : null} */}
+        {/* <Route exact path="/" component={} /> */}
+      </Switch>
     </div>
   );
 }
