b4f0443f7d56665e50a2ab2614d3886f007cd8dc
oliverclaxton <oliver.claxton66@gmail.com>
2020-11-27 11:23:04 +0100

    add caption for image functionality working

--- src/components/MySingleCampaignCard.js
+++ src/components/MySingleCampaignCard.js
@@ -9,7 +9,7 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { deleteCampaign } from "../store/campaigns/actions";
+import { addImageCaption } from "../store/campaigns/actions";
 import Form from "react-bootstrap/Form";
 import { Col } from "react-bootstrap";
 
@@ -30,27 +30,35 @@ const useStyles = makeStyles({
 const MySingleCampaignCard = (props) => {
   const classes = useStyles();
 
-  const [statusColor, setStatusColor] = useState("green");
+  const [status, setStatus] = useState(2);
   const [caption, setCaption] = useState("");
   const [imageId, setImageId] = useState("");
+
+  console.log("i am stauts", status);
+
+  const campaignId = props.id;
+
+  console.log(" i am props.id", props.id);
 
   const dispatch = useDispatch();
   const history = useHistory();
 
-  function submitForm(event) {
+  function submitForm(event, id, campaignId) {
     event.preventDefault();
+    console.log(" i am image id", id);
 
-    // dispatch(
-    //   addCampaign(title, description, contractLink, briefLink, date, talent)
-    // );
+    dispatch(addImageCaption(caption, id, campaignId));
   }
 
-  //   const onDelete = (id) => {
-  //     console.log("deleting campaign!", id);
+  let statusText = "";
 
-  //     dispatch(deleteCampaign(id));
-  //     history.push("/campaigns");
-  //   };
+  //   if (status == 2) {
+  //     statusText = "In Progress";
+  //   } else if (status == 1) {
+  //     statusText = "Complete";
+  //   } else {
+  //     return;
+  //   }
 
   console.log("i am proppppspsppspspsps", props);
   if (!props.campaignImages) {
@@ -60,91 +68,101 @@ const MySingleCampaignCard = (props) => {
   console.log("i am caption with the image id", imageId, caption);
 
   return (
-    // <div>h1</div>
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardContent>
-          <Typography
-            className={classes.font}
-            gutterBottom
-            variant="h5"
-            component="h2"
-          >
-            {props.title}
-          </Typography>
-          <Typography
-            className={classes.font}
-            variant="body2"
-            color="textSecondary"
-            component="p"
-          >
-            Description: {props.description}
-          </Typography>
-          <Typography
-            className={classes.font}
-            variant="body2"
-            color="textSecondary"
-            component="p"
-          >
-            date to go live: {props.dueDate}
-          </Typography>
-          <Typography
-            className={classes.font}
-            variant="body2"
-            color="textSecondary"
-            component="p"
-          >
-            Status: {statusColor}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
+    <div>
+      <div>
+        <h1>{props.title}</h1>
+        <h5>{props.description}</h5>
+        {/* <h5>Current Status : {statusText}</h5> */}
+        <h6>Date to go live: {props.dueDate}</h6>
+        {/* <select onChange={(event) => setStatus(event.target.value)}>
+          <option>Mark as Complete</option>
+          <option value={1}>Complete</option>
+          <option value={2}>In Progress</option>
+        </select> */}
+        <ImageUploader campaignId={props.id} />
+      </div>
+
+      <div>
         {props.campaignImages.map((ci) => {
           //   console.log("i am CICIC", ci);
           return (
             <div key={ci.id}>
-              <p>image number : {ci.id}</p>
-              <CardMedia className={classes.media} image={ci.imageUrl} />
-              <p>{ci.caption}</p>
+              {/* <p>image number : {ci.id}</p> */}
+              <img src={ci.imageUrl} alt="image" />
+
+              {!ci.caption ? (
+                <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+                  <h1 className="mt-5 mb-5"></h1>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Control
+                      //   value={caption}
+                      onChange={(event) => setCaption(event.target.value)}
+                      type="text"
+                      placeholder="Enter caption"
+                      as="textarea"
+                      rows={3}
+                      required
+                    />
+                  </Form.Group>
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    onClick={(e) => {
+                      submitForm(e, ci.id, campaignId);
+                    }}
+                  >
+                    Add Caption
+                  </Button>
+                </Form>
+              ) : (
+                <div>
+                  <p>Caption : {ci.caption}</p>
+                </div>
+              )}
             </div>
           );
         })}
-      </CardActions>
-      <ImageUploader campaignId={props.id} />
-      <Form.Control
-        as="select"
-        value={imageId}
-        onChange={(event) => setImageId(event.target.value)}
-        required
-      >
-        <option value="">Select Image to add caption</option>
-        {props.campaignImages.map((ci) => {
-          //   console.log("i am HELLOOOO", ci);
-          return (
-            <option key={ci.id} value={ci.id}>
-              {ci.id}
-            </option>
-          );
-        })}
-      </Form.Control>
-      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
-        <h1 className="mt-5 mb-5"></h1>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Control
-            value={caption}
-            onChange={(event) => setCaption(event.target.value)}
-            type="text"
-            placeholder="Enter caption"
-            as="textarea"
-            rows={3}
-            required
-          />
-        </Form.Group>
-        <Button variant="contained" type="submit" onClick={submitForm}>
-          Add Caption
-        </Button>
-      </Form>
-    </Card>
+      </div>
+
+      {/* <div>
+        <Form.Control
+          as="select"
+          value={imageId}
+          onChange={(event) => setImageId(event.target.value)}
+          required
+        >
+          <option value="">Select Image to add caption</option>
+          {props.campaignImages.map((ci) => {
+            //   console.log("i am HELLOOOO", ci);
+            return (
+              <option key={ci.id} value={ci.id}>
+                {ci.id}
+              </option>
+            );
+          })}
+        </Form.Control>
+      </div> */}
+
+      {/* <div>
+        <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+          <h1 className="mt-5 mb-5"></h1>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control
+              value={caption}
+              onChange={(event) => setCaption(event.target.value)}
+              type="text"
+              placeholder="Enter caption"
+              as="textarea"
+              rows={3}
+              required
+            />
+          </Form.Group>
+          <Button variant="contained" type="submit" onClick={submitForm}>
+            Add Caption
+          </Button>
+        </Form>
+      </div> */}
+    </div>
   );
 };
 
