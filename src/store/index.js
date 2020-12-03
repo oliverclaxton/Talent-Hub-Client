006e57d3f8af349fe2fda067b69bd7af85eb920f
oliverclaxton <oliver.claxton66@gmail.com>
2020-12-03 09:40:34 +0100

    removed dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,10 @@ import reducer from "./rootReducer";
 const devTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
+const enhancer = compose(applyMiddleware(ReduxThunk));
 
 const store = createStore(reducer, enhancer);
 
 export default store;
+
+// , devTools
